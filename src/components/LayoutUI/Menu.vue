950083f0550e84c9b8206501a82cd6ceb6d1de86
wing-0902 <114337707+te-wing@users.noreply.github.com>
2025-10-14 09:11:40 +0000

    すごいな．

--- src/components/LayoutUI/Menu.vue
+++ src/components/LayoutUI/Menu.vue
@@ -1,3 +1,60 @@
 <template>
-  あいうえお
+  <div class='menuContainer'>
+    <button
+      :class="{ 'is-active': isOpen }"
+      @click="toggleMenu"
+      aria-label="メニューを開閉"
+    >
+      <span class="bar"></span>
+      <span class="bar"></span>
+      <span class="bar"></span>
+    </button>
+    <nav></nav>
+    <div v-if="isOpen" class="overlay" @click="toggleMenu"></div>
+  </div>
 </template>
+
+<script lang="ts">
+  export default {
+    name: "HamburgerMenu",
+    data() {
+      return {
+        isOpen: false,
+      };
+    },
+    methods: {
+      toggleMenu() {
+        this.isOpen = !this.isOpen;
+      },
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+  .menuContainer {
+    position: relative;
+    z-index: 1000;
+
+    button {
+      width: 30px;
+      height: 30px;
+      background: none;
+      border: none;
+      padding: 0;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+      cursor: pointer;
+      z-index: 1001;
+      position: relative;
+
+      .bar {
+        display: block;
+        width: 100%;
+        height: 2px;
+        border-radius: 1px;
+        background-color: var(--foreground);
+      }
+    }
+  }
+</style>
