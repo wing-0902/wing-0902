a6b12a7f1a2fa62cf5bbb163f5eba1c15352f44a
wing-0902 <114337707+te-wing@users.noreply.github.com>
2025-10-15 03:26:26 +0000

    liでした

--- src/components/LayoutUI/Menu.vue
+++ src/components/LayoutUI/Menu.vue
@@ -32,7 +32,7 @@
         <li>
           <a>アプリ一覧</a>
           <ul>
-            <a href="https://github.com/wing-0902">GitHubプロフィール</a>
+            <li><a href="https://github.com/wing-0902">GitHubプロフィール</a></li>
           </ul>
         </li>
       </ul>
