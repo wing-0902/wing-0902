d8f20c804bddadeb16c8d71e4c8b89efb31ad649
wing-0902 <114337707+te-wing@users.noreply.github.com>
2025-10-16 13:53:30 +0000

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,12 @@ import { defineConfig, passthroughImageService } from 'astro/config';
 import svelte from '@astrojs/svelte';
 import vue from '@astrojs/vue';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), vue()],
+  site: 'https://wing.osaka',
+  integrations: [svelte(), vue(), sitemap()],
   image: {
     service: passthroughImageService()
   },
