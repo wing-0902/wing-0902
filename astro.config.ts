dbb32809a716b8f382c9ce4a0c1000cbe26f0dfe
Wing <114337707+wing-0902@users.noreply.github.com>
2026-07-08 10:27:23 +0000

    template [ci skip]

--- astro.config.ts
+++ astro.config.ts
@@ -5,7 +5,14 @@ import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [UnoCSS({
-    injectReset: false // disable auto style reset in order to specify multiple stylesheets
-  }), vue()]
+  integrations: [
+    UnoCSS({
+      injectReset: false // disable auto style reset in order to specify multiple stylesheets
+    }),
+    vue()
+  ],
+  output: 'static',
+  scopedStyleStrategy: 'class',
+  site: 'https://wing.osaka',
+  trailingSlash: 'always'
 });
