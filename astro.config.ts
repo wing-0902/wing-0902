8c813017028a9b24fb2a1802466187056233e3f3
Wing <114337707+wing-0902@users.noreply.github.com>
2026-08-01 01:03:28 +0000

    vueの削除，markdownプロセッサの変更

--- astro.config.ts
+++ astro.config.ts
@@ -1,26 +1,22 @@
 import { defineConfig } from 'astro/config';
 
+import { unified } from '@astrojs/markdown-remark';
+
 import UnoCSS from 'unocss/astro';
-import vue from '@astrojs/vue';
-
 import alpinejs from '@astrojs/alpinejs';
-import rehypeExternalLinks from 'rehype-external-links';
-
 import svelte from '@astrojs/svelte';
+
+import rehypeExternalLinks from 'rehype-external-links';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [UnoCSS(), vue(), alpinejs(), svelte()],
+  integrations: [UnoCSS(), alpinejs(), svelte()],
   markdown: {
-    rehypePlugins: [
-      [
-        rehypeExternalLinks,
-        {
-          target: '_blank',
-          rel: ['nofollow', 'noopener', 'noreferrer']
-        }
+    processor: unified({
+      rehypePlugins: [
+        [rehypeExternalLinks, { target: '_blank', rel: ['nofollow', 'noopener', 'noreferrer'] }]
       ]
-    ]
+    }),
   },
   output: 'static',
   scopedStyleStrategy: 'class',
